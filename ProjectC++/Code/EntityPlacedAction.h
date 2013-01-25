17a503e78cb85ab3446100d0be1e8597a7600b09
Alexander Ivannikov <Alexivan_91@hotmail.com>
2013-01-25 10:36:14 +0100

    Forgot to push EntityPlacedAction.h.

---
+++ ProjectC++/Code/EntityPlacedAction.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Action.h"
+#include "World.h"
+#include <Vector.h>
+
+
+
+class EntityPlacedAction : public Action
+{
+	Vector3 zPos, zRot, zScale;
+	unsigned int zEntID;
+	Entity* zEntCreated;
+	World* zWorld;
+public:
+	EntityPlacedAction(
+		World* world,
+		const unsigned int& EntID,
+		const Vector3& pos,
+		const Vector3& rot,
+		const Vector3& scale 
+	) :
+		zWorld(world),
+		zEntID(EntID),
+		zPos(pos),
+		zRot(rot),
+		zScale(scale)
+	{
+	}
+
+	virtual void Execute()
+	{
+		zEntCreated = zWorld->CreateEntity(zEntID);
+		zEntCreated->SetPosition(zPos);
+		zEntCreated->SetRotation(zRot);
+		zEntCreated->SetScale(zScale);
+	}
+
+	virtual void Undo()
+	{
+		if ( zEntCreated )
+		{
+			zWorld->RemoveEntity( zEntCreated );
+			zEntCreated = 0;
+		}
+	}
+};
